a95ff7153ccfef81c64b458ec86061477591186e
Mohammed hmedat <hmedat2008@gmail.com>
2018-04-27 17:27:18 +0300

    add more test cases for MovieInteractor

--- app/src/test/java/com/movie/app/MovieInteractorTest.kt
+++ app/src/test/java/com/movie/app/MovieInteractorTest.kt
@@ -6,13 +6,13 @@ import com.movie.app.interactors.IMoviesInteractor
 import com.movie.app.interactors.MoviesInteractor
 import com.movie.app.modules.Movie
 import com.movie.app.modules.MovieSearchFilter
+import com.nhaarman.mockito_kotlin.whenever
 import io.reactivex.Observable
 import io.reactivex.observers.TestObserver
 import org.junit.Before
 import org.junit.Test
 import org.mockito.ArgumentMatchers.any
 import org.mockito.Mock
-import org.mockito.Mockito.`when`
 import org.mockito.MockitoAnnotations
 
 
@@ -25,7 +25,6 @@ class MovieInteractorTest {
     @Mock
     private lateinit var movieSearchFilter: MovieSearchFilter
 
-
     @Before
     fun setUp() {
         MockitoAnnotations.initMocks(this)
@@ -35,40 +34,58 @@ class MovieInteractorTest {
     @Test
     fun testFindMovie() {
         val movie: Movie = Movie().apply {
-            id = 1010
+            id = 1
             title = "Avengers"
         }
         val testSubscriber = TestObserver<Movie>()
-        `when`(apiInterface.findMovie(any(Long::class.java))).thenReturn(Observable.just(movie))
+        whenever(apiInterface.findMovie(any(Long::class.java))).thenReturn(Observable.just(movie))
         moviesInteractor.findMovie(movie.id).subscribe(testSubscriber)
         testSubscriber.assertNoErrors()
+        testSubscriber.assertComplete()
         testSubscriber.assertValue(movie)
     }
 
     @Test
+    fun testFailureFindMovie() {
+        val exception = IllegalArgumentException()
+        val testSubscriber = TestObserver<Movie>()
+        whenever(apiInterface.findMovie(any(Long::class.java))).thenReturn(Observable.error(exception))
+        moviesInteractor.findMovie(10).subscribe(testSubscriber)
+        testSubscriber.assertError(exception)
+        testSubscriber.assertNotComplete()
+    }
+
+    @Test
     fun testGetMovies() {
-        val movie01: Movie = Movie().apply {
+        val result = LatestMoviesResult()
+        result.results = listOf(Movie().apply {
             id = 1
             title = "Avengers 01"
-        }
-        val movie02: Movie = Movie().apply {
+        }, Movie().apply {
             id = 2
             title = "Avengers 02"
-        }
-        val movie03: Movie = Movie().apply {
+        }, Movie().apply {
             id = 3
             title = "Avengers 03"
-        }
-
-        val result = LatestMoviesResult()
-        result.results = listOf(movie01, movie02, movie03)
-
+        })
         val testSubscriber = TestObserver<LatestMoviesResult>()
-        `when`(apiInterface.getLatestMovies(any(Int::class.java)))
+        whenever(apiInterface.getLatestMovies(any(Int::class.java)))
                 .thenReturn(Observable.just(result))
-
         moviesInteractor.getLatest(movieSearchFilter).subscribe(testSubscriber)
         testSubscriber.assertNoErrors()
+        testSubscriber.assertComplete()
         testSubscriber.assertValue(result)
+    }
+
+
+    @Test
+    fun testFailureGetMovies() {
+        val exception = IllegalArgumentException()
+        val testSubscriber = TestObserver<LatestMoviesResult>()
+        whenever(apiInterface.getLatestMovies(any(Int::class.java)))
+                .thenReturn(Observable.error(exception))
+        moviesInteractor.getLatest(movieSearchFilter).subscribe(testSubscriber)
+        testSubscriber.assertError(exception)
+        testSubscriber.assertNotComplete()
     }
 }
