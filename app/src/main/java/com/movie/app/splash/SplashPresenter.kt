641e595fba7ed0efbbf26d382ee9eece3562b0f3
Mohammed hmedat <hmedat2008@gmail.com>
2018-05-09 21:29:21 +0300

    Add splash screen

---
+++ app/src/main/java/com/movie/app/splash/SplashPresenter.kt
@@ -0,0 +1,24 @@
+package com.movie.app.splash
+
+import io.reactivex.Completable
+import io.reactivex.disposables.CompositeDisposable
+import java.util.concurrent.TimeUnit
+
+
+class SplashPresenter constructor(private val view: SplashActivityContractor.View)
+    : SplashActivityContractor.Presenter {
+
+    private val compositeDisposable: CompositeDisposable = CompositeDisposable()
+
+    override fun subscribe() {
+        Completable.complete()
+                .delay(3, TimeUnit.SECONDS)
+                .doOnComplete { view.startNextActivity(null) }
+                .subscribe()
+    }
+
+    override fun unSubscribe() {
+        compositeDisposable.clear()
+    }
+
+}
