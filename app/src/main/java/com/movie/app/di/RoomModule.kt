67e96daa614eefb1d271a18331ceddd5df1450bb
Mohammed hmedat <hmedat2008@yahoo.com>
2018-08-25 14:08:10 +0300

    remove allow queries on main thread

--- app/src/main/java/com/movie/app/di/RoomModule.kt
+++ app/src/main/java/com/movie/app/di/RoomModule.kt
@@ -19,9 +19,6 @@ class RoomModule {
     @Singleton
     fun provideDatabase(application: MyApp): AppDatabase {
         return Room.databaseBuilder(application, AppDatabase::class.java, "movie-database")
-                // allow queries on the main thread.
-                // Don't do this on a real app! See PersistenceBasicSample for an example.
-                .allowMainThreadQueries()
                 .build()
     }
 
